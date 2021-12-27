23b02975e918d034a5a41d71402e5b07ad62f6bb
wayclub <youngha.park.en@gmail.com>
2021-12-27 23:25:24 +0900

    initial framework + get input from user

--- cryptoprice.py
+++ cryptoprice.py
@@ -11,7 +11,10 @@ import requests_cache
 
 #Market id to call
 ### need to get input for ticker
-MARKET_ID = "bitcoin"
+
+#Example: MARKET_ID = "bitcoin"
+MARKET_ID = input("Enter name of crypto for price info(must enter in full name, lower case): ")
+day_input = input("Get price data from how many days ago? ")
 
 #payload inputs are vs_currency(usd) and 
 #days(Minutely data for duration within 1 day, Hourly data for duration between 1 day and 90 days, Daily data for duration above 90 days)
@@ -22,15 +25,10 @@ payload = {
 
 def main():
     r = getcoinprice(payload)
-    #print(r.status_code)
     f_df = convert_to_df(r)
     print(f_df)
-    print(f_df.loc[str(datetime.today().date() - timedelta(days=180))]["Price"].item())
-    print(f_df.loc[str(datetime.today().date() - timedelta(days=30))]["Price"].item())
-    print(f_df.loc[str(datetime.today().date() - timedelta(days=7))]["Price"].item())
-    print(f_df.loc[str(datetime.today().date() - timedelta(days=1))]["Price"].item())
-    #print(datetime.today().date() - timedelta(days=30))
-    
+    print(f_df.loc[str(datetime.today().date() - timedelta(days=int(day_input)))]["Price"].item())
+            
 def getcoinprice(payload):
     #Coingecko API => get coin price data for specific market id
     url = "https://api.coingecko.com/api/v3/coins/" + MARKET_ID + "/market_chart"
@@ -38,6 +36,7 @@ def getcoinprice(payload):
     response = requests.get(url, params=payload)
     if response.status_code != 200:
         print(response.text)
+        print("Crypto name of " + MARKET_ID + " does not exist.")
     else:
         #print(response.json())
         return response
@@ -56,18 +55,21 @@ def convert_to_df(response):
     #format date data from timestamp/epoch to datetime
     df["Date"] = pd.to_datetime(df["Date"], unit="ms")#.dt.date
     #drop second to last row with the most recent today data
-    #df = df.drop(df.index[-2])
+    df = df.drop(df.index[-2])
     #set date to index
     df = df.set_index(["Date"], inplace=False)
+    
+    return df
+
     #df.info()
     #df.describe()
     #print(type(df.index))
     #print(df.loc['2020-10-25'])
-    
-    return df
 
 if __name__ == "__main__":
     main()
+
+
 
 
 """def jprint(obj):
